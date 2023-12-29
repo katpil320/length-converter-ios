ece9a056aecf18d9989da33e4f20b786b7acdfdc
Martin Liptak <katpil320@icloud.com>
2023-12-29 09:17:04 +0100

    Initial Commit

---
+++ Konvrtr/KonvrtrApp.swift
@@ -0,0 +1,17 @@
+//
+//  KonvrtrApp.swift
+//  Konvrtr
+//
+//  Created by Martin Lipták on 29/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct KonvrtrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
